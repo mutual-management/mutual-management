2496041f7b5fc400ae1f412302ba1fc8a562a550
y-yanagii <yyanagisawa555@gmail.com>
2021-10-21 21:30:23 +0900

    Add: scheduleコントローラ作成

---
+++ app/controllers/schedule_controller.rb
@@ -0,0 +1,7 @@
+class ScheduleController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'schedule/new'
+  get 'schedule/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "application#hello"
   # Almost every application defines a route for the root path ("/") at the top of this file.
