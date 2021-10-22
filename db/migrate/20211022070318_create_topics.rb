6259bce15888b37f6a1a979e1d81a58dff02b3b7
yukiHaga <oceansthirteen7510@gmail.com>
2021-10-22 16:08:01 +0900

    Add: usersテーブルとtopicsテーブルを作成

--- db/migrate/20211022070318_create_topics.rb
+++ db/migrate/20211022070318_create_topics.rb
@@ -3,7 +3,7 @@ class CreateTopics < ActiveRecord::Migration[7.0]
     create_table :topics do |t|
       t.text :content, null: false
       t.string :month
-      t.references :user, null: false, foreign_key: true
+      t.references :users, null: false, foreign_key: true
 
       t.timestamps
     end
