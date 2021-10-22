2ef968b01716fc23d0e34f7ebde9bd12a5aed9d0
yukiHaga <oceansthirteen7510@gmail.com>
2021-10-22 15:48:42 +0900

    Add: UserモデルとTopicモデルの追加

---
+++ db/migrate/20211022060734_create_topics.rb
@@ -0,0 +1,11 @@
+class CreateTopics < ActiveRecord::Migration[7.0]
+  def change
+    create_table :topics do |t|
+      t.text :content, null: false
+      t.string :month
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
