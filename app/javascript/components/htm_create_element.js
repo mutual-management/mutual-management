e054b3f142bd7cb4c3e510f51ad43995a705d069
yunosuke924 <y.minegishi0924@gmail.com>
2021-10-24 12:15:40 +0900

    ADD : reactの検証

---
+++ app/javascript/components/htm_create_element.js
@@ -0,0 +1,4 @@
+// JSXの代替
+import { createElement } from "react"
+import htm from "htm"
+export default htm.bind(createElement)
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,10 @@ pin "@hotwired/turbo-rails", to: "turbo.js"
 pin "@hotwired/stimulus", to: "stimulus.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+pin 'htm', to: "https://cdn.esm.sh/v45/htm@3.1.0/es2021/htm.js"
+pin "react", to: "https://ga.jspm.io/npm:react@17.0.2/index.js"
+pin "react-dom", to: "https://ga.jspm.io/npm:react-dom@17.0.2/index.js"
+pin "object-assign", to: "https://ga.jspm.io/npm:object-assign@4.1.1/index.js"
+pin "scheduler", to: "https://ga.jspm.io/npm:scheduler@0.20.2/index.js"
+pin_all_from 'app/javascript/components', under: 'components'
