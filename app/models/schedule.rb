92ec159451023a08f55b60ca15da9fd629ac9321
y-yanagii <yyanagisawa555@gmail.com>
2021-10-25 21:29:46 +0900

    label_colorのenumを定義

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -4,5 +4,7 @@ class Schedule < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :time, presence: true,
   validates :labelColor, presence: true,
-  validates :date, presence: true,
+  validates :date, presence: true
+
+  enum labelColor: { red: 0, blue: 1, yellow: 2, puple: 3, green: 4 }
 end
