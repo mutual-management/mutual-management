af4e4bf8db2e9b7a4c830113cda3e76062dd74d3
y-yanagii <yyanagisawa555@gmail.com>
2021-10-25 21:09:33 +0900

    スケジュールモデル実装（動作未確認）

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,8 @@
 class Schedule < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :time, presence: true,
+  validates :labelColor, presence: true,
+  validates :date, presence: true,
 end
