17468e542addbe7ad6603ab5cc72f02986133388
yukiHaga <oceansthirteen7510@gmail.com>
2021-10-26 17:02:06 +0900

    Add: userとtopicを生成するseedファイルを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+1.times do
+  User.create(
+      name: "yoshiyuki",
+      email: "yoshiyuki@gmail.com",
+      password: 'yoshiyuki',
+      password_confirmation: 'yoshiyuki'
+  )
+end
+
+10.times do
+  Topic.create(
+      content: "test",
+      month: "2020-11-09",
+      user_id: 1
+  )
+end
