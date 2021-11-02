3971460e5538d24ce67a84bdd8965ab992d66358
yukiHaga <oceansthirteen7510@gmail.com>
2021-11-02 16:53:49 +0900

    Add: tailwindの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,10 +2,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap';
+import "../css/tailwindcss.css";
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-require("stylesheets/application")
