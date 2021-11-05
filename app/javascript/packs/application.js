a13727eb774dbaa8831768403854852db08f5197
ryuji55 <rt.october.2@icloud.com>
2021-11-05 17:04:26 +0900

    Add:ゴミ箱の表示を整えた

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,11 @@
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 import "../css/tailwindcss.css";
+import '@fortawesome/fontawesome-free/js/all';
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
 
