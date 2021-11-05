3868e3016e5a4ba7c955de128ec432a517b01561
yasuhitoiida <yasuhitoiida830@gmail.com>
2021-11-05 17:34:44 +0900

    Add:daily_stacks/newページの基本的なデザインを構築

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :daily_stacks, only: %i[index new show create update destroy]
   resources :expense_categories, only: %i[index create destroy]
-  root "application#hello"
   # Almost every application defines a route for the root path ("/") at the top of this file.
   # root "articles#index"
 end
