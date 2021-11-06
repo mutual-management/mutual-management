e0b69505edf46cdf5d6ecfcdaa69de3d12b1dcff
y-yanagii <yyanagisawa555@gmail.com>
2021-11-06 18:52:01 +0900

    ADD: React, TypeScriptの導入（Webpacker）

---
+++ app/javascript/components/HelloWorld.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+type Props = {
+  greeting: string
+}
+
+const HelloWorld: React.VFC<Props> = ({ greeting }) => {
+  return <>
+    Greetingts: {greeting}
+    <div>jeiwojfiewjfijewfiojieofjiowjiojwiefjiewo</div>
+    <h1>JJJJJJJJ</h1>
+  </>
+}
+
+export default HelloWorld
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "application#hello"
   resources :users
   resources :topics
+  resources :calendar
 end
