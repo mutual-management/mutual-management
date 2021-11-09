80f8c25ff09bb95df3039e757b8b75ef67a18775
ryuji55 <rt.october.2@icloud.com>
2021-11-09 16:52:36 +0900

    Fix:application.cssのデバッグ途中

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@ import * as ActiveStorage from "@rails/activestorage";
 import "channels";
 import "../css/tailwindcss.css";
 import '@fortawesome/fontawesome-free/js/all';
-require("jquery")
+require("jquery");
 require("stylesheets/application");
 
 Rails.start();
