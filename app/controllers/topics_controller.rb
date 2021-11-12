86ca9ad757f63739c075a01f31d5ea536b4d03ee
ryuji55 <rt.october.2@icloud.com>
2021-11-12 16:01:39 +0900

    Add:カード追加機能実装

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,12 +1,24 @@
 class TopicsController < ApplicationController
   def show; end
   def index
+    @topic = Topic.new
     @month = params[:month] || Time.zone.today.all_month
     @topics = Topic.where(month: @month)
+  end
+
+  def create
+    @topic = Topic.create(topic_params)
+    redirect_to topics_path
   end
 
   def destroy
     @topic = Topic.find(params[:id])
     @topic.delete
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:content, :month).merge(user_id: 1)
   end
 end
