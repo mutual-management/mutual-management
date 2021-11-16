91c349cfaf4d5e8bec6173a1c334e278a4595640
yasuhitoiida <yasuhitoiida830@gmail.com>
2021-11-16 13:48:17 +0900

    Add:DailyStack新規登録機能 #12

--- app/controllers/daily_stacks_controller.rb
+++ app/controllers/daily_stacks_controller.rb
@@ -1,11 +1,15 @@
 class DailyStacksController < ApplicationController
-  before_action :set_daily_stack, except: %i[index new create]
+  before_action :set_daily_stack, only: %i[show update destroy]
 
   def index
+    current_user = User.first # ログイン機能が実装されたら削除する行
     daily_stacks = current_user.daily_stacks.all
   end
 
   def new
+    current_user = User.first # ログイン機能が実装されたら削除する行
+    @date = Date.new(2021,11,4)
+    @daily_stack = current_user.daily_stacks.build
   end
 
   def show
@@ -13,8 +17,9 @@ class DailyStacksController < ApplicationController
   end
 
   def create
-    daily_stack = current_user.daily_stacks.build(daily_stack_params)
-    daily_stack.create
+    current_user = User.first # ログイン機能が実装されたら削除する行
+    @daily_stack = current_user.daily_stacks.build(daily_stack_params)
+    @daily_stack.save! # エラーハンドリング未実装
   end
 
   def update
@@ -32,6 +37,6 @@ class DailyStacksController < ApplicationController
   end
 
   def daily_stack_params
-    params.require(:daily_stack).permit(:title, :amount, :status, :date, :unit)
+    params.require(:daily_stack).permit(:title, :amount, :status, :date, :unit, :expense_category_id)
   end
 end
--- app/models/daily_stack.rb
+++ app/models/daily_stack.rb
@@ -5,9 +5,9 @@ class DailyStack < ApplicationRecord
   enum status: { todo: 0, done: 1 }
   enum unit: { study: 0, expense: 1, others: 2 }
 
-  validate :title, presense: true, length: { maximum: 50 }
-  validate :status, presense: true
-  validate :date, presense: true
-  validate :unit, presense: true
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :status, presence: true
+  validates :date, presence: true
+  validates :unit, presence: true
 
 end
