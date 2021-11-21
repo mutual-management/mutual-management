123fd1e8396eb34ce1727552f9af6e6026d71928
y-yanagii <yyanagisawa555@gmail.com>
2021-11-21 20:32:37 +0900

    Add: jQueryをローカルに導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,9 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+require('jquery')
+require('modal.js')
 // Support component names relative to this directory:
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
-require('jquery')
-require('modal.js')
