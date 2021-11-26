76470f617571b9b4e24dea1d854c452841e0d779
ryuji55 <rt.october.2@icloud.com>
2021-11-26 15:38:58 +0900

    Add:動的に生成したid属性を持たせて編集可能にした

--- app/javascript/src/edit_modal.js
+++ app/javascript/src/edit_modal.js
@@ -2,22 +2,23 @@
 
 jQuery(function($){
     $('.remodal-edit').click(function(){
+       let edit_id = $(this).attr('id');
         //classでonを持っているかチェック
-        if(!$('#edit_content').hasClass('on')){
+        if(!$(`#edit_content${edit_id}`).hasClass('on')){
             //編集可能時はclassでonをつける
-            $('.edit_content').addClass('on');
-            let txt = $('.edit_content').text();
+            $(`#edit_content${edit_id}`).addClass('on');
+            let txt = $(`#edit_content${edit_id}`).text();
             //テキストをinputのvalueに入れてで置き換え
-            $('.edit_content').html('<input class="bg-gray-700" type="text" value="'+txt+'" />');
+            $(`#edit_content${edit_id}`).html('<input class="bg-gray-700" type="text" value="'+txt+'" />');
             //同時にinputにフォーカスをする
             $('dd > input').focus().blur(function(){
-                let inputVal = $('.edit_content').val();
+                let inputVal = $(`#edit_content${edit_id}`).val();
                 //もし空欄だったら空欄にする前の内容に戻す
                 if(inputVal===''){
-                    inputVal = $('.edit_content').defaultValue;
+                    inputVal = $(`#edit_content${edit_id}`).defaultValue;
                 };
                 //編集が終わったらtextで置き換える
-                $('.edit_content').parent().removeClass('on').text(inputVal);
+                $(`#edit_content${edit_id}`).parent().removeClass('on').text(inputVal);
             });
         };
     });
