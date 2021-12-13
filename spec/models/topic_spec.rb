ec62ede44efa077f6d140bed2b1f937a9b9301fb
ryuji55 <rt.october.2@icloud.com>
2021-12-13 16:12:37 +0900

    Add:itの中身を書いた

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Topic, type: :model do
   describe 'バリデーション' do
+    let (:topic) { build(:topic) }
     context '正常系' do
-      it '全項目入力してあればtopicを投稿できる' do
+      it '全項目入力されていれば投稿できる' do
+        expect(topic.valid?).to eq(true)
       end
     end
     context '異常系' do
