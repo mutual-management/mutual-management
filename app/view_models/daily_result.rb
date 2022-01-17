c05d27038869fef4135ce9e2a9981e0cb7d471d9
y-yanagii <yyanagisawa555@gmail.com>
2022-01-18 00:28:26 +0900

    ViewModelクラスの設計

--- app/view_models/daily_result.rb
+++ app/view_models/daily_result.rb
@@ -1,5 +1,20 @@
 class DailyResult
+  attr_accessor :title, :date, :amount, :unit, :status
+
+  # statusenum
+
   def initialize()
+    # 支出の合計の配列をもらう？
+    # タイトル、日付、値（予定は時間？）、status(予定か日々か支出か)、
     # TODO 予定と日々の結果と支出の結果をそれぞれ共通のクラスで扱えるように設定
+    # 初期化
+  end
+
+  def sum_expenses
+    # その日の支出を足す
+  end
+
+  def check_status
+    # ステータス(予定か日々か支出か)、
   end
 end
